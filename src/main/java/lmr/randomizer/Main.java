ca862dba3e37d9163e430ce1e24d9e822c5fa7f1
thezerothcat <thezerothcat@gmail.com>
2017-08-07 22:10:26 -0600

    Don't build main project in dev mode!

--- src/main/java/lmr/randomizer/Main.java
+++ src/main/java/lmr/randomizer/Main.java
@@ -28,7 +28,7 @@ import java.util.List;
  */
 public class Main {
     public static void main(String[] args) {
-        if(true) {
+        if(false) {
 //            Settings.startingSeed = 1246445508;
             Settings.randomizeShops = true;
             Settings.nonRandomizedItems.add("Holy Grail");
