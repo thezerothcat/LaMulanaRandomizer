63fe5e4a98c4f72c2a3c13f548653d39392cbb67
Tim Köhlmann <tim.koehlmann@crono.co.uk>
2018-10-08 20:21:29 +0200

    Forgot ChestGraphics.java

---
+++ src/main/java/lmr/randomizer/random/ChestGraphics.java
@@ -0,0 +1,14 @@
+package lmr.randomizer.random;
+
+/**
+ * Enum class for treasure chest sprite configuration
+ * @author Tim Köhlmann (tkoehlmann)
+ *
+ */
+
+public enum ChestGraphics {
+    KEEP,           ///< Chests stay as they are
+    ALLCOIN,        ///< All chests appear as coin chests 
+    ALLTREASURE,    ///< All chests appear as treasure chests
+    ALLRANDOM,      ///< All chests are random
+}
