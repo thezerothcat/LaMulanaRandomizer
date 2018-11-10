84a20075c8108fd989cce349d641fc9f3e7150b9
thezerothcat <thezerothcat@gmail.com>
2018-11-09 18:12:37 -0700

    Fix access logic for map chest in extinction

--- src/main/java/lmr/randomizer/FileUtils.java
+++ src/main/java/lmr/randomizer/FileUtils.java
@@ -13,7 +13,7 @@ import java.util.*;
  * Created by thezerothcat on 7/10/2017.
  */
 public class FileUtils {
-    public static final String VERSION = "1.42.1";
+    public static final String VERSION = "1.43.0";
 
     private static BufferedWriter logWriter;
     private static final List<String> KNOWN_RCD_FILE_HASHES = new ArrayList<>();
