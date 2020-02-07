096c1afa5188ce6ff4d5bef6e6abd37f7766ff8d
thezerothcat <thezerothcat@gmail.com>
2020-02-06 18:11:57 -0700

    Version update

--- src/main/java/lmr/randomizer/FileUtils.java
+++ src/main/java/lmr/randomizer/FileUtils.java
@@ -19,7 +19,7 @@ import java.util.zip.ZipInputStream;
  * Created by thezerothcat on 7/10/2017.
  */
 public class FileUtils {
-    public static final String VERSION = "2.23.0";
+    public static final String VERSION = "2.24.0";
     public static final int EXISTING_FILE_WIDTH = 1024;
     public static final int EXISTING_FILE_HEIGHT = 512;
     public static final int GRAPHICS_VERSION = 4;
