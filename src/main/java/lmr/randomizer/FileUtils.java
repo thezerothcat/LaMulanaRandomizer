28c1ebc643460a6a307b9cfb6d289ac797fd8817
thezerothcat <thezerothcat@gmail.com>
2022-07-29 20:55:13 -0600

    Updated glitch logic for Viy access

--- src/main/java/lmr/randomizer/FileUtils.java
+++ src/main/java/lmr/randomizer/FileUtils.java
@@ -18,7 +18,7 @@ import java.util.zip.ZipInputStream;
  * Created by thezerothcat on 7/10/2017.
  */
 public class FileUtils {
-    public static final String VERSION = "2.35.0";
+    public static final String VERSION = "2.36.0";
 
     private static BufferedWriter logWriter;
     private static final Map<String, String> KNOWN_RCD_FILE_HASHES = new HashMap<>();
